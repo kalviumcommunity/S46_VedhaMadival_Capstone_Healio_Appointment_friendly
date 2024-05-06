3698528718b0405f44322ceeff827a81cac751e5
vedha <vedh1305@gmail.com>
2024-05-06 11:31:03 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 require('dotenv').config();
+const userRoute = require("./Routes/userRoute");
 
+app.use("/", userRoute);
 
 app.get('/' , (req , res)=>{
     res.json({message:"Capstone Get request successful"})
