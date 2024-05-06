ba929a61519bbc11b3d10800cfef356e0560d6ba
vedha <vedh1305@gmail.com>
2024-05-06 14:07:19 +0530

    Database Schema Created

---
+++ Backend/Models/UserModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isdoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isadmin:{
+      type: Boolean,
+      default: false,
+    },
+    seenNotifications: {
+      type: Array,
+      default : []
+    },
+    unseenNotifications: {
+      type: Array,
+      default : []
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
